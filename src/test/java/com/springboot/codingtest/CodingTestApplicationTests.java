3af3cd01aba4bcf69fffbd56f79753a3196cde05
rasmithe1 <rasmiranjan.pradhan01@gmail.com>
2021-07-21 19:12:18 +0530

    scr added

---
+++ src/test/java/com/springboot/codingtest/CodingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.codingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
